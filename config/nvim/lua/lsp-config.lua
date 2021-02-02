9dc3c6309a5c7c9900ca9a080db200545cdb9402
Thomas Burk <t.burk@bckx.de>
2021-02-02 23:39:58 +0100

    Add json and java lsp config

--- config/nvim/lua/lsp-config.lua
+++ config/nvim/lua/lsp-config.lua
@@ -2,6 +2,11 @@ require'lspconfig'.angularls.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.hls.setup{}
+require'lspconfig'.jsonls.setup{}
+require'lspconfig'.jdtls.setup{
+  cmd = {'jdt.ls', '/usr/local/share/jdt.ls/workspaces/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')},
+  root_dir = require'lspconfig'.util.root_pattern(".git", "pom.xml", "mvnw")
+}
 require'lspconfig'.perlls.setup{}
 require'lspconfig'.r_language_server.setup{}
 require'lspconfig'.rust_analyzer.setup{}
