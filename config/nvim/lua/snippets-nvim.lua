09df0b4e57e04a44e2169229863c5e2ff38e9104
Thomas Burk <t.burk@bckx.de>
2021-03-06 18:51:35 +0100

    Add sample snippets date, todo and note

--- config/nvim/lua/snippets-nvim.lua
+++ config/nvim/lua/snippets-nvim.lua
@@ -1,1 +1,10 @@
 require'snippets'.use_suggested_mappings()
+
+require'snippets'.snippets = {
+    _global = {
+        date = function() return os.date() end,
+        todo = [[TODO(${1=io.popen("id -un"):read"*l"}): ]],
+        note = [[NOTE(${1=io.popen("id -un"):read"*l"}): ]]
+    }
+}
+
